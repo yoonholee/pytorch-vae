fix analytic kl

main.py
@@ -51,20 +51,28 @@ def test(epoch):
     writer.add_scalar('test/LL', get_loss_k(elbos, -1), epoch)
     return get_loss_k(elbos, args.importance_num)
 
+mean_img = (train_loader.dataset.train_data.type(torch.float) / 255).mean(0).reshape(-1).numpy()
 model = VAE(device, x_dim=args.x_dim, h_dim=args.h_dim, z_dim=args.z_dim,
-            beta=args.beta, no_analytic_kl=args.no_analytic_kl).to(device)
+            beta=args.beta, analytic_kl=args.analytic_kl, mean_img=mean_img).to(device)
 optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, eps=1e-4)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=10**(-1/7))
 
 train_step = 0
-#learning_rates = [1e-3 * 10**-(j/7) for j in sum([[i] * 3**i for i in range(8)], [])]
-#for epoch in range(1, len(learning_rates)+1):
-    #optimizer.param_groups[0]['lr'] = learning_rates[epoch - 1]
-for epoch in range(1, args.epochs):
-    writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], epoch)
-    train(epoch)
-    with torch.no_grad():
-        test_loss = test(epoch)
-        scheduler.step(test_loss)
-        if args.figs and epoch % 10 == 1: draw_figs(model, args, test_loader, epoch)
+if args.iwae_lr:
+    learning_rates = [1e-3 * 10**-(j/7) for j in sum([[i] * 3**i for i in range(8)], [])]
+    for epoch in range(1, len(learning_rates)+1):
+        optimizer.param_groups[0]['lr'] = learning_rates[epoch - 1]
+        writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], epoch)
+        train(epoch)
+        with torch.no_grad():
+            test_loss = test(epoch)
+            if args.figs and epoch % 10 == 1: draw_figs(model, args, test_loader, epoch)
+else:
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', cooldown=20, factor=10**(-1/7))
+    for epoch in range(1, args.epochs):
+        writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], epoch)
+        train(epoch)
+        with torch.no_grad():
+            test_loss = test(epoch)
+            scheduler.step(test_loss)
+            if args.figs and epoch % 10 == 1: draw_figs(model, args, test_loader, epoch)
 

vae.py
@@ -5,11 +5,11 @@ from torch.distributions.normal import Normal
 from torch.distributions.bernoulli import Bernoulli
 
 class VAE(nn.Module):
-    def __init__(self, device, x_dim, h_dim, z_dim, beta, no_analytic_kl):
+    def __init__(self, device, x_dim, h_dim, z_dim, beta, analytic_kl, mean_img):
         super(VAE, self).__init__()
         self.proc_data = lambda x: x.to(device).reshape(-1, x_dim)
         self.beta = beta
-        self.no_analytic_kl = no_analytic_kl
+        self.analytic_kl = analytic_kl
         self.encoder = nn.Sequential(
             nn.Linear(x_dim, h_dim), nn.Tanh(),
             nn.Linear(h_dim, h_dim), nn.Tanh())
@@ -26,12 +26,15 @@ class VAE(nn.Module):
                 torch.nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain('tanh'))
                 m.bias.data.fill_(.01)
         self.apply(init)
+        mean_img = np.clip(mean_img, 1e-8, 1. - 1e-7)
+        mean_img_logit = np.log(mean_img / (1. - mean_img))
+        self.decoder[-1].bias = torch.nn.Parameter(torch.Tensor(mean_img_logit))
 
     def encode(self, x):
         x = self.proc_data(x)
         h = self.encoder(x)
         mu, _std = self.enc_mu(h), self.enc_sig(h)
-        std = torch.exp(.5 * _std)
+        std = nn.functional.softplus(_std) #std = torch.exp(.5 * _std)
         return Normal(mu, std)
 
     def decode(self, z):
@@ -47,14 +50,15 @@ class VAE(nn.Module):
         true_x = self.proc_data(true_x)
         lpxz = x_dist.log_prob(true_x).sum(-1) # equivalent to binary cross entropy.
 
-        if self.no_analytic_kl:
+        if self.analytic_kl and is_vae:
+            # SGVB^B: -KL(q(z|x)||p(z)) + log p(x|z). Use when KL can be done analytically.
+            assert z.size(0) == 1 and z.size(1) == 1
+            kl = torch.distributions.kl.kl_divergence(z_dist, self.prior).sum(-1)
+        else:
             # SGVB^A: log p(z) - log q(z|x) + log p(x|z)
             lpz = self.prior.log_prob(z).sum(-1)
             lqzx = z_dist.log_prob(z).sum(-1)
             kl = -lpz + lqzx
-        else:
-            # SGVB^B: -KL(q(z|x)||p(z)) + log p(x|z). Use when KL can be done analytically.
-            kl = torch.distributions.kl.kl_divergence(z_dist, self.prior).sum(-1)
         return -kl + lpxz
 
     def logmeanexp(self, inputs, dim=1):
@@ -70,7 +74,7 @@ class VAE(nn.Module):
         x_dist = self.decode(z)
 
         elbo = self.elbo(true_x, z, x_dist, z_dist) # mean_n, imp_n, batch_size
-        elbo_iwae = self.logmeanexp(elbo, 1) # mean_n, batch_size
+        elbo_iwae = self.logmeanexp(elbo, 1).squeeze(1) # mean_n, batch_size
         elbo_iwae_m = torch.mean(elbo_iwae, 0) # batch_size
         return {'elbo': elbo, 'loss': -elbo_iwae_m}
 